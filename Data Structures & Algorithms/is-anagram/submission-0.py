cca13180ca2b144b555d36f57e4aba3f1cf3055f
NoobCoder-dweeb <81848583+NoobCoder-dweeb@users.noreply.github.com>
2026-03-30 14:31:01 +0800

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        cnt1 = {k: 0 for k in s}
+        cnt2 = {k: 0 for k in t}
+        
+        for c in s:
+            cnt1[c] +=1
+        for c in t:
+            cnt2[c] +=1
+
+        return cnt1 == cnt2
